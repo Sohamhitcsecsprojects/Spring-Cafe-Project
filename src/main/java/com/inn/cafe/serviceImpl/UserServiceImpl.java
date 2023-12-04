ec7b0cc3529ba6be4d528583abb9a541ecb4685e
Soham Maity <soham086.hitcsecs2021@gmail.com>
2023-12-04 11:48:02 +0530

    first commit

---
+++ src/main/java/com/inn/cafe/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,68 @@
+package com.inn.cafe.serviceImpl;
+
+import com.inn.cafe.constents.CafeConstants;
+import com.inn.cafe.dao.UserDao;
+import com.inn.cafe.model.User;
+import com.inn.cafe.service.UserService;
+import com.inn.cafe.utils.CafeUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Objects;
+
+@Slf4j
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserDao userDao;
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+      log.info("inside Signup {}", requestMap);
+
+      try {
+          if (validateSignUpMap(requestMap)) {
+
+              User user = userDao.findByEmailId(requestMap.get("email"));
+              if (Objects.isNull(user)) {
+                  userDao.save(getUserFromMap(requestMap));
+                  return CafeUtils.getResponseEntity("Successfully Registered", HttpStatus.OK);
+              } else {
+                  return CafeUtils.getResponseEntity("Email Already Exists", HttpStatus.BAD_REQUEST);
+
+              }
+          } else {
+              return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
+          }
+      }catch (Exception ex){
+          ex.printStackTrace();
+      }
+      return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private User getUserFromMap(Map<String, String> requestMap) {
+        User user = new User();
+        user.setName(requestMap.get("name"));
+        user.setContactNumber(requestMap.get("contactNumber"));
+        user.setEmail(requestMap.get("email"));
+        user.setPassword(requestMap.get("password"));
+        user.setStatus("false");
+        user.setRole("user");
+        return user;
+    }
+
+    private boolean validateSignUpMap(Map<String, String> requestMap) {
+        if(requestMap.containsKey("name") && requestMap.containsKey("contactNumber")
+                && requestMap.containsKey("email") && requestMap.containsKey("password"))
+        {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
